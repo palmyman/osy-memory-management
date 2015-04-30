22592ed09fefd481ddebc556f00dff7efe255a99
Lubomir Cuhel <cuhel.lubomir@gmail.com>
2015-04-30 15:53:12 +0200

    init nb project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: palmyman
+ *
+ * Created on April 30, 2015, 3:48 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
